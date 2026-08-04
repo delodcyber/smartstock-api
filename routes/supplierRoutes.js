616603b39cdccc7aa41144217358ed51d4e8e029
delodcyber <Truebasisoflife@yahoo.com>
2026-08-04 18:12:04 +0100

    feat: add routes for categories, products, and suppliers with CRUD operations

---
+++ routes/supplierRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+    getAllSuppliers,
+    getSupplierById,
+    createSupplier,
+    updateSupplier,
+    deleteSupplier
+} from "../controllers/supplierController.js";
+
+const router = express.Router();
+
+router.get("/", getAllSuppliers);
+
+router.get("/:id", getSupplierById);
+
+router.post("/", createSupplier);
+
+router.put("/:id", updateSupplier);
+
+router.delete("/:id", deleteSupplier);
+
+
+export { router as supplierRoutes };
