90d4105554b7376cce96cb61ea3b794fbb36de09
delodcyber <Truebasisoflife@yahoo.com>
2026-08-04 18:13:02 +0100

    feat: add auth and authorize middleware, and integrate category routes in server

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import { productRoutes } from "./routes/productRoutes.js";
+import { supplierRoutes } from "./routes/supplierRoutes.js";
+import { categoryRoutes } from "./routes/categoryRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 connectDB();
 
-app.use(express.json());
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/suppliers", supplierRoutes);
+app.use("/api/v1/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 
