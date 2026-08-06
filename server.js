48f8986e24c24df655d135e461c81de49ff261dc
delodcyber <Truebasisoflife@yahoo.com>
2026-08-06 16:02:58 +0100

    feat: add Swagger JSDoc integration and update package.json with new dependencies

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./config/db.js";
 import { productRoutes } from "./routes/productRoutes.js";
 import { supplierRoutes } from "./routes/supplierRoutes.js";
 import { categoryRoutes } from "./routes/categoryRoutes.js";
-
-dotenv.config();
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
 
 const app = express();
 
@@ -16,6 +17,8 @@ connectDB();
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/suppliers", supplierRoutes);
 app.use("/api/v1/categories", categoryRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 5000;
 
